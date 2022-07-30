78372852dd470c876bdc861429f9eabd8583a1fb
SSAFY <wj5295@naver.com>
2022-07-30 20:26:00 +0900

    회원가입 로그인 폼 제작 및 회원가입 자바스크립트 작성

---
+++ src/main/java/com/won/blog/controller/api/UserApiController.java
@@ -0,0 +1,7 @@
+package com.won.blog.controller.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApiController {
+}
---
+++ src/main/java/com/won/blog/dto/ResponseDto.java
@@ -0,0 +1,2 @@
+package com.won.blog.dto;public class ResponseDto {
+}
