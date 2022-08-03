903d5d33964b2b2c24ea4b4c660d0c58fd454edd
wonjaechoi <wj5295@naver.com>
2022-08-03 22:14:19 +0900

    예외 모아서 처리

--- src/main/java/com/won/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/won/blog/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.won.blog.handler;
 
+import com.won.blog.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GlobalExceptionHandler {
     //Exception.class하면 모든 예외 여기로 모인다.
-    @ExceptionHandler(value = IllegalArgumentException.class)//IllegalArgumentException이 발생하면 에러를 이 함수에 전달
-    public String handleArgumentException(IllegalArgumentException e) {
-        return "<h1>"+e.getMessage()+"</h1>";
+    @ExceptionHandler(value = Exception.class/*IllegalArgumentException.class*/)//IllegalArgumentException이 발생하면 에러를 이 함수에 전달
+    public ResponseDto<String> handleArgumentException(Exception e) {
+
+        return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
     }
+
+
 
 
 }
