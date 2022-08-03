a4e53548a35b36a331fc9ce31c9037f8686fb4f1
wonjaechoi <wj5295@naver.com>
2022-08-03 22:16:07 +0900

    JPA 네이밍 쿼리

--- src/main/java/com/won/blog/repository/UserRepository.java
+++ src/main/java/com/won/blog/repository/UserRepository.java
@@ -2,10 +2,17 @@ package com.won.blog.repository;
 
 import com.won.blog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 //DAO
 //자동으로 빈으로 등록도미.
 //@Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer > { //User테이블을 관리하고 이 테이블의 키는 정수다
+    //JPA Naming 쿼리 : 실제로 이런 함수는 없지만
+    //이게 자동으로 실행 : SELECT * FROM user  WHERE username=? AND password=?
+    User findByUsernameAndPassword(String username, String password);
 
+    /*위에 껄 더 자주 사용 이렇게 간단한 경우
+    @Query(value = "SELECT * FROM user  WHERE username=? AND password=?", nativeQuery = true)
+    User login(String username, String password);*/
 }
